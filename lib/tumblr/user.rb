9ef218aa3a21b752bb8da884e3a5491253d6d46f
John Crepezzi <john.crepezzi@gmail.com>
2013-02-04 15:03:15 -0500

    Clean up user

--- lib/tumblr/user.rb
+++ lib/tumblr/user.rb
@@ -1,40 +1,40 @@
 module Tumblr
   class Client
     module User
+
       def info
-        info = get("v2/user/info")
+        info = get('v2/user/info')
       end
 
-      def dashboard(options={})
+      def dashboard(options = {})
         valid_opts = [:limit, :offset, :type, :since_id, :reblog_info, :notes_info]
-        if valid_options(valid_opts, options)
-          get("v2/user/dashboard", options)
-        end
+        valid_options(valid_opts, options)
+        get('v2/user/dashboard', options)
       end
 
-      def likes(offset=0, limit=20)
-        likes = get("v2/user/likes", {:limit => limit, :offset => offset})
+      def likes(offset = 0, limit = 20)
+        likes = get('v2/user/likes', :limit => limit, :offset => offset)
       end
 
-      def following(offset=0, limit=20)
-        following = get("v2/user/following", {:limit => limit, :offset => offset})
+      def following(offset = 0, limit = 20)
+        get('v2/user/following', :limit => limit, :offset => offset)
       end
 
       def follow(url)
-        follow = post("v2/user/follow", {:url => url})
+        post('v2/user/follow', :url => url)
       end
-    
+
       def unfollow(url)
-        follow = post("v2/user/unfollow", {:url => url})
+        post('v2/user/unfollow', :url => url)
       end
 
       def like(id, reblog_key)
-        like = post("v2/user/like", {:id => id, :reblog_key => reblog_key})
+        post('v2/user/like', :id => id, :reblog_key => reblog_key)
       end
 
       def unlike(id, reblog_key)
-        unlike = post("v2/user/unlike", {:id => id, :reblog_key => reblog_key})
-      end 
+        post('v2/user/unlike', :id => id, :reblog_key => reblog_key)
+      end
     end
   end
 end
