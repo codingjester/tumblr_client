0dac6c0ece135eab5c58beb61c6f0ca3a1b473d0
John Crepezzi <john.crepezzi@gmail.com>
2013-02-04 15:10:30 -0500

    Cleanup in OAuth

--- lib/tumblr/request/oauth.rb
+++ lib/tumblr/request/oauth.rb
@@ -8,7 +8,7 @@ module Tumblr
   module Request
     class TumblrOAuth < Faraday::Middleware
       def call(env)
-        if env[:method].to_s == "get"
+        if env[:method].to_s == 'get'
             params = Faraday::Utils.parse_query(env[:url].query) || {}
             url = "#{env[:url].scheme}://#{env[:url].host}#{env[:url].path}"
         else
@@ -19,10 +19,9 @@ module Tumblr
         params.each do |key, value|
           signature_params = {} if value.respond_to?(:content_type)
         end
-        env[:request_headers]["Authorization"] = self.oauth_gen(env[:method], url, signature_params) 
-        env[:request_headers]["Content-type"] = "application/x-www-form-urlencoded"                                                                              
-        env[:request_headers]["Host"] = @options[:api_host]
-        
+        env[:request_headers]['Authorization'] = self.oauth_gen(env[:method], url, signature_params)
+        env[:request_headers]['Content-type'] = 'application/x-www-form-urlencoded'
+        env[:request_headers]['Host'] = @options[:api_host]
 
         @app.call(env)
       end
@@ -37,33 +36,32 @@ module Tumblr
          params[:oauth_signature_method] = 'HMAC-SHA1'
          params[:oauth_timestamp] = Time.now.to_i
          params[:oauth_token] = @options[:token]
-         params[:oauth_version] = "1.0"
+         params[:oauth_version] = '1.0'
          params[:oauth_signature] = self.oauth_sig(method, url, params)
-         
+
          header = []
          params.each do |key, value|
-            if key.to_s.include?("oauth")
+            if key.to_s.include?('oauth')
               header << "#{key.to_s}=#{value}"
             end
          end
 
          "OAuth #{header.join(", ")}"
-
       end
-      
+
       def oauth_sig(method, url, params)
         parts = [method.upcase, URI.encode(url.to_s, /[^a-z0-9\-\.\_\~]/i)]
-        
+
         #sort the parameters
         params = Hash[params.sort_by{ |key, value| key.to_s}]
-        
+
         encoded = []
         params.each do |key, value|
             encoded << "#{key.to_s}=#{URI.encode(value.to_s, /[^a-z0-9\-\.\_\~]/i)}"
         end
 
-        parts << URI.encode(encoded.join("&"), /[^a-z0-9\-\.\_\~]/i)
-        signature_base = parts.join("&")
+        parts << URI.encode(encoded.join('&'), /[^a-z0-9\-\.\_\~]/i)
+        signature_base = parts.join('&')
         secret = "#{@options[:consumer_secret]}&#{@options[:token_secret]}"
         Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA1.new, secret, signature_base)).chomp.gsub(/\n/, '')
       end
