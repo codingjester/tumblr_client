556703cc53b957dfcea4739b3f06245bddb8817c
John Crepezzi <john.crepezzi@gmail.com>
2013-02-04 12:00:49 -0500

    Refactor post.rb to remove duplication
    Closes #26

--- lib/tumblr/helpers.rb
+++ lib/tumblr/helpers.rb
@@ -1,14 +1,22 @@
 module Tumblr
   class Client
     module Helper
-      
+
       def valid_options(valid_opts, opts)
         bad_opts = opts.select { |val| !valid_opts.include?(val) }
         if !bad_opts.empty?
-          raise Exception, "Invalid options passed, Only #{valid_opts} allowed."
+          raise Exception, "Invalid options (#{bad_opts.keys.join(', ')}) passed, only #{valid_opts} allowed."
         end
         return true
       end
+
+      def validate_no_collision(options, attributes)
+        count = attributes.count { |a| options.has_key?(a) }
+        if count > 1
+          raise ArgumentError.new "Can only use one of: #{attributes.join(', ')} (Found #{count})"
+        end
+      end
+
     end
   end
 end
--- lib/tumblr/post.rb
+++ lib/tumblr/post.rb
@@ -1,124 +1,111 @@
 module Tumblr
   class Client
     module Post
-      
-      @@standard_post_options  = [:state, :tags, :tweet, :date, :markdown, :slug, :format]
-      
-      def edit(blog_name, options={})
-        post("v2/blog/#{blog_name}/post/edit", options)  
+
+      STANDARD_POST_OPTIONS = [:state, :tags, :tweet, :date, :markdown, :slug, :format]
+
+      def edit(blog_name, options = {})
+        post("v2/blog/#{blog_name}/post/edit", options)
       end
 
-      #Don't be lazy and create a nice interface
-      def reblog(blog_name, options={})
-        post("v2/blog/#{blog_name}/post/reblog", options)  
+      def reblog(blog_name, options = {})
+        post("v2/blog/#{blog_name}/post/reblog", options)
       end
-      
+
       def delete(blog_name, id)
-        post("v2/blog/#{blog_name}/post/delete", {:id => id})  
+        post("v2/blog/#{blog_name}/post/delete", :id => id)
       end
 
-      def photo(blog_name, options={})
-        valid_opts = @@standard_post_options + [:caption, :link, :data, :data_raw, :source, :photoset_layout]
-        if valid_options(valid_opts, options)
-          options[:type] = "photo"
-          if (options.has_key?(:data) && options.has_key?(:source))
-            raise Exception, "You can only use one parameter, either source or data."
-          end
-          if options.has_key?(:source) && options[:source].kind_of?(Array)
-            count = 0
-            options[:source].each do |src|
-              options["source[#{count}]"] = src
-              count += 1
-            end
-            options.delete(:source)
-          end
+      def photo(blog_name, options = {})
+        valid_opts = STANDARD_POST_OPTIONS + [:caption, :link, :data, :data_raw, :source, :photoset_layout]
+        valid_options(valid_opts, options)
+        validate_no_collision options, [:data, :source]
 
-          if options.has_key?(:data) && options.has_key?(:data_raw)
-            raise ArgumentError.new 'You can only use data or data_raw'
+        # Allow source to be passed as an Array
+        if options.has_key?(:source) && options[:source].kind_of?(Array)
+          options[:source].each.with_index do |src, idx|
+            options["source[#{idx}]"] = src
           end
+          options.delete(:source)
+        end
 
-          # Allow data to be passed as filenames
-          if options.has_key?(:data)
-            data = options.delete :data
-            data = [data] unless Array === data
-            data.each.with_index do |filepath, idx|
-              options["data[#{idx}]"] = File.open(filepath, 'rb').read
-            end
+        options[:type] = 'photo'
+        extract_data!(options)
+        post("v2/blog/#{blog_name}/post", options)
+      end
+
+      def quote(blog_name, options = {})
+        valid_opts = STANDARD_POST_OPTIONS + [:quote, :source]
+        valid_options(valid_opts, options)
+
+        options[:type] = 'quote'
+        post("v2/blog/#{blog_name}/post", options)
+      end
+
+      def text(blog_name, options = {})
+        valid_opts = STANDARD_POST_OPTIONS + [:title, :body]
+        valid_options(valid_opts, options)
+
+        options[:type] = 'text'
+        post("v2/blog/#{blog_name}/post", options)
+      end
+
+      def link(blog_name, options = {})
+        valid_opts = STANDARD_POST_OPTIONS + [:title, :url, :description]
+        valid_options(valid_opts, options)
+
+        options[:type] = 'link'
+        post("v2/blog/#{blog_name}/post", options)
+      end
+
+      def chat(blog_name, options = {})
+        valid_opts = STANDARD_POST_OPTIONS + [:title, :conversation]
+        valid_options(valid_opts, options)
+
+        options[:type] = 'chat'
+        post("v2/blog/#{blog_name}/post", options)
+      end
+
+      def audio(blog_name, options = {})
+        valid_opts = STANDARD_POST_OPTIONS + [:data, :caption, :external_url]
+        valid_options(valid_opts, options)
+        validate_no_collision options, [:data, :external_url]
+
+        options[:type] = 'audio'
+        extract_data!(options)
+        post("v2/blog/#{blog_name}/post", options)
+      end
+
+      def video(blog_name, options = {})
+        valid_opts = STANDARD_POST_OPTIONS + [:data, :embed, :caption]
+        valid_options(valid_opts, options)
+        validate_no_collision options, [:data, :embed]
+
+        options[:type] = 'video'
+        extract_data!(options)
+        post("v2/blog/#{blog_name}/post", options)
+      end
+
+      private
+
+      # Look for the various ways that data can be passed, and normalize
+      # the result in this hash
+      def extract_data!(options)
+        validate_no_collision options, [:data, :data_raw]
+        if options.has_key?(:data)
+          data = options.delete :data
+          data = [data] unless Array === data
+          data.each.with_index do |filepath, idx|
+            options["data[#{idx}]"] = File.open(filepath, 'rb').read
           end
-
-          # Allow raw data to be passed in also
-          if options.has_key?(:data_raw)
-            data_raw = options.delete :data_raw
-            data_raw = [data_raw] unless Array === data_raw
-            data_raw.each.with_index do |dr, idx|
-              options[:"data[#{idx}]"] = dr
-            end
+        elsif options.has_key?(:data_raw)
+          data_raw = options.delete :data_raw
+          data_raw = [data_raw] unless Array === data_raw
+          data_raw.each.with_index do |dr, idx|
+            options[:"data[#{idx}]"] = dr
           end
-
-          post("v2/blog/#{blog_name}/post", options)
         end
       end
-
-      def quote(blog_name, options={})
-        valid_opts = @@standard_post_options + [:quote, :source]
-        if valid_options(valid_opts, options)
-          options[:type] = "quote"
-          post("v2/blog/#{blog_name}/post", options)
-        end
-      end
-
-      def text(blog_name, options={})
-        valid_opts = @@standard_post_options + [:title, :body]
-        if valid_options(valid_opts, options)
-          options[:type] = "text"
-          post("v2/blog/#{blog_name}/post", options)
-        end
-      end
-
-      def link(blog_name, options={})
-        valid_opts = @@standard_post_options + [:title, :url, :description]
-        if valid_options(valid_opts, options)
-          options[:type] = "link"
-          post("v2/blog/#{blog_name}/post", options)
-        end
-      end
-
-      def chat(blog_name, options={})
-        valid_opts = @@standard_post_options + [:title, :conversation]
-        if valid_options(valid_opts, options)
-          options[:type] = "chat"
-          post("v2/blog/#{blog_name}/post", options)
-        end
-      end
-
-      def audio(blog_name, options={})
-        valid_opts = @@standard_post_options + [:data, :caption, :external_url]
-        if valid_options(valid_opts, options)
-          options[:type] = "audio"
-          if (options.has_key?(:data) && options.has_key?(:external_url))
-            raise Exception, "You can only use one parameter, either data or external url."
-          end
-          if(options.has_key?(:data))
-            options[:data] = File.open(options[:data],'rb').read()
-          end
-          post("v2/blog/#{blog_name}/post", options)
-        end
-      end
-      
-      def video(blog_name, options={})
-        valid_opts = @@standard_post_options + [:data, :embed, :caption]
-        if valid_options(valid_opts, options)
-          options[:type] = "video"
-          if (options.has_key?(:data) && options.has_key?(:embed))
-            raise Exception, "You can only use one parameter, either data or embed."
-          end
-          if(options.has_key?(:data))
-            options[:data] = File.open(options[:data],'rb').read()
-          end
-          post("v2/blog/#{blog_name}/post", options)
-        end
-      end
-
     end
   end
 end
