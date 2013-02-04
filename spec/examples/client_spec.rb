a2bff5b7b4ac1db1c7a973e5a27ec019b495da5d
John Crepezzi <john.crepezzi@gmail.com>
2013-02-04 15:40:33 -0500

    Some clients specs

---
+++ spec/examples/client_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe Tumblr::Client do
+
+  context 'when using the generic copy' do
+
+    before do
+      @key = 'thekey'
+      Tumblr.configure do |c|
+        c.consumer_key = @key
+      end
+    end
+
+    it 'should give new clients those credentials' do
+      client = Tumblr::Client.new
+      client.credentials[:consumer_key].should == @key
+    end
+
+    it 'should have it\'s own credentials' do
+      Tumblr.credentials[:consumer_key].should == @key
+    end
+
+  end
+
+  context 'when using custom copies of the client' do
+
+    before do
+      @client1 = Tumblr::Client.new(:consumer_key => 'key1')
+      @client2 = Tumblr::Client.new(:consumer_key => 'key2')
+    end
+
+    it 'should keep them separate' do
+      [
+        @client1.credentials[:consumer_key],
+        @client2.credentials[:consumer_key]
+      ].uniq.count.should == 2
+    end
+
+  end
+
+end
---
+++ spec/examples/tagged_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Tumblr::Client::Tagged do
+
+  let(:client) { Tumblr::Client.new }
+  let(:consumer_key) { 'consumer' }
+  let(:tag)          { 'helloworld' }
+
+  before do
+    Tumblr.configure do |c|
+      c.consumer_key = consumer_key
+    end
+  end
+
+  describe :tagged do
+
+    before do
+      client.should_receive(:get).once.with('v2/tagged', {
+        :tag => tag,
+        :api_key => consumer_key
+      }).and_return('response')
+    end
+
+    it 'should setup the request properly' do
+      r = client.tagged tag
+      r.should == 'response'
+    end
+
+  end
+
+end
